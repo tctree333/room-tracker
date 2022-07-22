d0a935fd1ff4e26aebafb91136a09f45a0b2e379
Tomi Chen <tctree333@users.noreply.github.com>
2022-07-22 16:58:21 -0700

    add gauges for real time visualization

---
+++ src/lib/components/Dial.svelte
@@ -0,0 +1,101 @@
+<script lang="ts">
+	export let value: number;
+	export let min: number;
+	export let max: number;
+	export let ranges: Record<string, [number, number, string]>;
+	export let title: string;
+	export let unit: string;
+
+	$: [label, color] = findRange(value);
+
+	function findRange(value: number): [string, string] {
+		for (const [label, [min, max, color]] of Object.entries(ranges)) {
+			if (value >= min && value <= max) {
+				return [label, color];
+			}
+		}
+		return ['', '#22C55E'];
+	}
+</script>
+
+<article>
+	<h2>{title}</h2>
+	<meter class="sr-only" {value} {min} {max}>{value}{unit}</meter>
+	<svg aria-hidden="true" viewBox="0 0 340 340">
+		<circle
+			class="outline"
+			r="150"
+			cx="50%"
+			cy="50%"
+			stroke="#A1A1AA"
+			stroke-width="40"
+			fill="none"
+			pathLength="1000"
+			stroke-dasharray="811"
+		/>
+		<circle
+			class="filled"
+			r="150"
+			cx="50%"
+			cy="50%"
+			stroke={color}
+			stroke-width="30"
+			fill="none"
+			pathLength="1000"
+			stroke-dasharray="{800 * Math.min(Math.max((value - min) / (max - min), 0.02), 1)} 1000"
+		/>
+		<text class="value" x="50%" y="48%" text-anchor="middle" dominant-baseline="middle"
+			>{value.toFixed(2)}</text
+		>
+		<text class="unit" x="50%" y="68%" text-anchor="middle" dominant-baseline="middle">{unit}</text>
+	</svg>
+	<p>{label}</p>
+</article>
+
+<style>
+	article {
+		padding: 16px;
+		border: 2px solid #d6d3d1;
+		border-radius: 8px;
+	}
+	h2 {
+		margin: 0;
+		text-align: center;
+		margin-bottom: 12px;
+		font-weight: 400;
+		font-size: 1.5rem;
+	}
+	p {
+		margin: 0;
+		text-align: center;
+		font-size: 1.25rem;
+	}
+	svg {
+		color: #22c55e;
+		display: block;
+		width: 100%;
+	}
+	svg circle {
+		transform-origin: center;
+	}
+	svg circle.filled {
+		transform: rotate(126deg);
+		transition-property: stroke-dasharray;
+		transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+		transition-duration: 300ms;
+	}
+	svg circle.outline {
+		transform: rotate(124deg);
+	}
+	svg text {
+		font: inherit;
+		color: black;
+	}
+	svg text.value {
+		font-size: 3.5rem;
+	}
+	svg text.unit {
+		font-size: 2rem;
+		font-weight: 300;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
 	import '$lib/global.css';
 
+	import Dial from '$lib/components/Dial.svelte';
 	import { browser } from '$app/env';
-	import type { DataEndpointPayload, RoomDataPayload } from 'src/lib/types';
+	import type { DataEndpointPayload, RoomDataPayload } from '$lib/types';
 
 	let messages: DataEndpointPayload[] = [];
-	let currentState: RoomDataPayload;
-	let lastUpdated = '';
+	let currentState: RoomDataPayload = {
+		PM1: 0,
+		'PM2.5': 0,
+		PM4: 0,
+		PM10: 0,
+		HUM1: 0,
+		TMP1: 0,
+		VOC: 0,
+		NOX: 0
+	};
+	let lastUpdated = 'never';
 
 	if (browser) {
 		const sse = new EventSource('/data');
@@ -26,7 +36,125 @@
 	}
 </script>
 
-<h1>Wow!</h1>
+<main class="content">
+	<h1>Room Air Data</h1>
 
-<p>Last updated at {lastUpdated}.</p>
-<pre>{JSON.stringify(currentState, undefined, 2)}</pre>
+	<p>Last updated at {lastUpdated}.</p>
+	<pre>{JSON.stringify(currentState, undefined, 2)}</pre>
+</main>
+<section>
+	<div class="content">
+		<h2>Real-Time</h2>
+	</div>
+	<div class="grid">
+		<Dial
+			title={'PM1.0'}
+			unit={'µg/m³'}
+			value={currentState?.PM1 || 0}
+			min={0}
+			max={500}
+			ranges={{
+				'': [0, 500, '#D946EF']
+			}}
+		/>
+		<Dial
+			title={'PM2.5'}
+			unit={'µg/m³'}
+			value={currentState?.['PM2.5'] || 0}
+			min={0}
+			max={500}
+			ranges={{
+				'': [0, 500, '#D946EF']
+			}}
+		/>
+		<Dial
+			title={'PM4.0'}
+			unit={'µg/m³'}
+			value={currentState?.PM4 || 0}
+			min={0}
+			max={500}
+			ranges={{
+				'': [0, 500, '#D946EF']
+			}}
+		/>
+		<Dial
+			title={'PM10.0'}
+			unit={'µg/m³'}
+			value={currentState?.PM10 || 0}
+			min={0}
+			max={500}
+			ranges={{
+				'': [0, 500, '#D946EF']
+			}}
+		/>
+		<Dial
+			title={'Temperature'}
+			unit={'°F'}
+			value={(currentState?.TMP1 || 0) * (9 / 5) + 32}
+			min={25}
+			max={110}
+			ranges={{
+				Freezing: [0, 30, '#3B82F6'],
+				Cold: [30, 45, '#0EA5E9'],
+				Cool: [45, 65, '#14B8A6'],
+				Comfortable: [65, 75, '#22C55E'],
+				Warm: [75, 90, '#EAB308'],
+				Hot: [90, 150, '#EF4444']
+			}}
+		/>
+		<Dial
+			title={'Humidity'}
+			unit={'%'}
+			value={currentState?.HUM1 || 0}
+			min={0}
+			max={100}
+			ranges={{
+				Dry: [0, 30, '#EAB308'],
+				Comfortable: [30, 60, '#22C55E'],
+				Wet: [60, 100, '#06B6D4']
+			}}
+		/>
+		<Dial
+			title={'VOC'}
+			unit={'/500'}
+			value={currentState?.VOC || 0}
+			min={0}
+			max={500}
+			ranges={{
+				Low: [0, 150, '#22C55E'],
+				Mild: [150, 250, '#EAB308'],
+				Medium: [250, 400, '#F97316'],
+				High: [400, 500, '#EF4444']
+			}}
+		/>
+		<Dial
+			title={'NOX'}
+			unit={'/500'}
+			value={currentState?.NOX || 0}
+			min={0}
+			max={500}
+			ranges={{
+				Low: [0, 20, '#22C55E'],
+				Mild: [20, 150, '#EAB308'],
+				Medium: [150, 300, '#F97316'],
+				High: [300, 500, '#EF4444']
+			}}
+		/>
+	</div>
+</section>
+
+<style>
+	.content {
+		max-width: 720px;
+		margin: 0 auto;
+		padding: 16px;
+	}
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(min(200px, 100%), 1fr));
+		grid-gap: 16px;
+	}
+	section {
+		padding: 16px;
+	}
+</style>
